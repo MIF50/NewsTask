0b90065307e98c8c3cb2c34649d217ca966f0929
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-02 18:42:04 +0300

    Initial Commit

---
+++ NewsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsTask
+//
+//  Created by Mohamed Ibrahim on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
