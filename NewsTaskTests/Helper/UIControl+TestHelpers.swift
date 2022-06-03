cd32538f9140133a54c9b318f267edc076db7d04
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-03 10:19:20 +0300

    Extract duplicate logic into a reusable UIControl extension

---
+++ NewsTaskTests/Helper/UIControl+TestHelpers.swift
@@ -0,0 +1,18 @@
+//
+//  UIControl+TestHelpers.swift
+//  NewsTaskTests
+//
+//  Created by Mohamed Ibrahim on 03/06/2022.
+//
+
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
