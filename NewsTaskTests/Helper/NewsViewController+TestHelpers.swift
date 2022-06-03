c04f78fdf61e8c76a51ed3ffb15bf0d6a0fdecf5
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-03 10:22:43 +0300

    Extract NewsViewController test helpers to a separate file

---
+++ NewsTaskTests/Helper/NewsViewController+TestHelpers.swift
@@ -0,0 +1,60 @@
+//
+//  NewsViewController+TestHelpers.swift
+//  NewsTaskTests
+//
+//  Created by Mohamed Ibrahim on 03/06/2022.
+//
+
+import UIKit
+import NewsTask
+
+extension NewsViewController {
+    func simulateUserInitiatedNewsReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    @discardableResult
+    func simulateNewsImageViewVisible(at index: Int)-> NewsImageCell? {
+        return newsImageView(at: index) as? NewsImageCell
+    }
+    
+    func simulateNewsImageViewNotVisible(at row: Int) {
+        let view = simulateNewsImageViewVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: newsImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+    }
+    
+    func simulateNewsImageViewNearVisible(at row: Int) {
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: newsImagesSection)
+        ds?.tableView(tableView, prefetchRowsAt: [index])
+    }
+    
+    func simulateNewsImageViewNotNearVisible(at row: Int) {
+        simulateNewsImageViewNearVisible(at: row)
+        
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: newsImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
+    
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+    
+    func numberOfRenderedNewsImageViews() -> Int {
+        return tableView.numberOfRows(inSection: newsImagesSection)
+    }
+    
+    func newsImageView(at row: Int) -> UITableViewCell? {
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: newsImagesSection)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
+    
+    private var newsImagesSection: Int {
+        return 0
+    }
+}
--- NewsTaskTests/NewsViewControllerTests.swift
+++ NewsTaskTests/NewsViewControllerTests.swift
@@ -348,57 +348,6 @@ class NewsViewControllerTests: XCTestCase {
     }
 }
 
-private extension NewsViewController {
-    func simulateUserInitiatedNewsReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    @discardableResult
-    func simulateNewsImageViewVisible(at index: Int)-> NewsImageCell? {
-        return newsImageView(at: index) as? NewsImageCell
-    }
-    
-    func simulateNewsImageViewNotVisible(at row: Int) {
-        let view = simulateNewsImageViewVisible(at: row)
-        
-        let delegate = tableView.delegate
-        let index = IndexPath(row: row, section: newsImagesSection)
-        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-    }
-    
-    func simulateNewsImageViewNearVisible(at row: Int) {
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: newsImagesSection)
-        ds?.tableView(tableView, prefetchRowsAt: [index])
-    }
-    
-    func simulateNewsImageViewNotNearVisible(at row: Int) {
-        simulateNewsImageViewNearVisible(at: row)
-        
-        let ds = tableView.prefetchDataSource
-        let index = IndexPath(row: row, section: newsImagesSection)
-        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-    }
-    
-    var isShowingLoadingIndicator: Bool {
-        return refreshControl?.isRefreshing == true
-    }
-    
-    func numberOfRenderedNewsImageViews() -> Int {
-        return tableView.numberOfRows(inSection: newsImagesSection)
-    }
-    
-    func newsImageView(at row: Int) -> UITableViewCell? {
-        let ds = tableView.dataSource
-        let index = IndexPath(row: row, section: newsImagesSection)
-        return ds?.tableView(tableView, cellForRowAt: index)
-    }
-    
-    private var newsImagesSection: Int {
-        return 0
-    }
-}
-
 private extension NewsImageCell {
     func simulateRetryAction() {
         newsImageRetryButton.simulateTap()
