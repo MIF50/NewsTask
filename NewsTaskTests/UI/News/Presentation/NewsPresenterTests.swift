c4d305ae5ebb491362aa5942c79cf7184d072ad2
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-04 13:37:06 +0300

    NewsPresenter displays loading on didStartLoadingNews

--- NewsTaskTests/UI/News/Presentation/NewsPresenterTests.swift
+++ NewsTaskTests/UI/News/Presentation/NewsPresenterTests.swift
@@ -16,19 +16,30 @@ struct NewsErrorViewModel {
     }
 }
 
+struct NewsLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol NewsLoadingView {
+    func display(_ viewModel: NewsLoadingViewModel)
+}
+
 protocol NewsErrorView {
     func display(_ viewModel: NewsErrorViewModel)
 }
 
 final class NewsPresenter {
+    private let loadingView: NewsLoadingView
     private let errorView: NewsErrorView
     
-    init(errorView: NewsErrorView) {
+    init(loadingView: NewsLoadingView, errorView: NewsErrorView) {
+        self.loadingView = loadingView
         self.errorView = errorView
     }
     
     func didStartLoadingNews() {
         errorView.display(.noError)
+        loadingView.display(NewsLoadingViewModel(isLoading: true))
     }
 }
 
@@ -41,33 +52,41 @@ final class NewsPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
-    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+    func test_didStartLoadingNews_displaysNoErrorMessageAndStartsLoading() {
         let (sut, view) = makeSUT()
         
         sut.didStartLoadingNews()
         
-        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+        XCTAssertEqual(view.messages, [
+            .display(errorMessage: .none),
+            .display(isLoading: true)
+        ])
     }
     
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: NewsPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = NewsPresenter(errorView: view)
+        let sut = NewsPresenter(loadingView: view,errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy: NewsErrorView {
+    private class ViewSpy: NewsLoadingView, NewsErrorView {
         enum Message: Equatable {
             case display(errorMessage: String?)
+            case display(isLoading: Bool)
         }
         
         private(set) var messages = [Message]()
         
         func display(_ viewModel: NewsErrorViewModel) {
             messages.append(.display(errorMessage: viewModel.message))
+        }
+        
+        func display(_ viewModel: NewsLoadingViewModel) {
+            messages.append(.display(isLoading: viewModel.isLoading))
         }
     }
     
