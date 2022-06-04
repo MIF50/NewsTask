1f2148bde205a6eba9a970c2c70531d7a30e11a4
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-04 12:54:58 +0300

    Move NewsImageDataLoaderPresentationAdapter to its own file

--- NewsTask/UI/News/Composers/NewsUIComposer.swift
+++ NewsTask/UI/News/Composers/NewsUIComposer.swift
@@ -42,35 +42,3 @@ private extension NewsViewController {
         return newsController
     }
 }
-
-final class NewsImageDataLoaderPresentationAdapter<View: NewsImageView, Image>: NewsImageCellControllerDelegate where View.Image == Image {
-    private let model: NewsImage
-    private let imageLoader: NewsImageDataLoader
-    private var task: NewsImageDataLoaderTask?
-    
-    var presenter: NewsImagePresenter<View, Image>?
-    
-    init(model: NewsImage, imageLoader: NewsImageDataLoader) {
-        self.model = model
-        self.imageLoader = imageLoader
-    }
-    
-    func didRequestImage() {
-        presenter?.didStartLoadingImageData(for: model)
-        
-        let model = self.model
-        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-                
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        }
-    }
-    
-    func didCancelImageRequest() {
-        task?.cancel()
-    }
-}
