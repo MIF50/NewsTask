3f95cc2dc58cf3fc1b8e578200f93b362257a53f
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2022-06-04 13:31:59 +0300

    NewsPresenter displays no error on didStartLoadingNews

--- NewsTaskTests/UI/News/Presentation/NewsPresenterTests.swift
+++ NewsTaskTests/UI/News/Presentation/NewsPresenterTests.swift
@@ -8,11 +8,31 @@
 import XCTest
 import NewsTask
 
-final class NewsPresenter {
-    init(view: Any) {
+struct NewsErrorViewModel {
+    let message: String?
 
+    static var noError: NewsErrorViewModel {
+        return NewsErrorViewModel(message: nil)
     }
 }
+
+protocol NewsErrorView {
+    func display(_ viewModel: NewsErrorViewModel)
+}
+
+final class NewsPresenter {
+    private let errorView: NewsErrorView
+    
+    init(errorView: NewsErrorView) {
+        self.errorView = errorView
+    }
+    
+    func didStartLoadingNews() {
+        errorView.display(.noError)
+    }
+}
+
+
 final class NewsPresenterTests: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
@@ -21,18 +41,34 @@ final class NewsPresenterTests: XCTestCase {
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
+    func test_didStartLoadingFeed_displaysNoErrorMessage() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoadingNews()
+        
+        XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: NewsPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = NewsPresenter(view: view)
+        let sut = NewsPresenter(errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
     
-    private class ViewSpy {
-        let messages = [Any]()
+    private class ViewSpy: NewsErrorView {
+        enum Message: Equatable {
+            case display(errorMessage: String?)
+        }
+        
+        private(set) var messages = [Message]()
+        
+        func display(_ viewModel: NewsErrorViewModel) {
+            messages.append(.display(errorMessage: viewModel.message))
+        }
     }
     
 }
